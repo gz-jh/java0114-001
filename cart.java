b8615b545c9679afdeb07afb06c3d94264428268
gzjh <gz950516@163.com>
2019-06-29 10:28:49 +0800

    edit user.java

--- cart.java
+++ cart.java
@@ -12,6 +12,7 @@ import java.util.Date;
 public class cart implements Serializable {
     private Integer id;
     private String name;
+	//this is edit**************************************
     private Float price;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createdate;
