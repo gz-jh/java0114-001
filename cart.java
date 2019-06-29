b34765432603713d9c7b047a03e71089f6a9454d
gzjh <gz950516@163.com>
2019-06-29 10:10:23 +0800

    add cart.java

---
+++ cart.java
@@ -0,0 +1,18 @@
+package com.offcn.springbootdemo.po;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data//get set tostring
+@AllArgsConstructor//有参构造
+@NoArgsConstructor
+public class cart implements Serializable {
+    private Integer id;
+    private String name;
+    private Float price;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date createdate;
+}
